b6185849751ab5f3d878bf078a8177e8e3ad9065
JairoRibera <jairosalvatierra098@gmail.com>
2026-04-14 12:31:48 +0200

    .

--- Unlucky_Boy/Assets/Code/Player/Armas/Disparo_Prov.cs
+++ Unlucky_Boy/Assets/Code/Player/Armas/Disparo_Prov.cs
@@ -5,14 +5,20 @@ using static UnityEngine.GraphicsBuffer;
 
 public class Disparo_Prov : MonoBehaviour
 {
+    //Aqui ponemos el scriptable object de la arma que se va a usar
     public armas arma;
     private Camera cam;
     public float range;
     public float damage;
     public float bullet;
+    //Collider de la hitbox
+    public GameObject hitbox;
+    public float Temporizador;
+    public float time = 0.5f;
     private void Start()
     {
         cam = Camera.main;
+        Temporizador = time;
     }
     void Update()
     {
@@ -25,8 +31,10 @@ public class Disparo_Prov : MonoBehaviour
 
     void Shoot()
     {
+        //Comrpobamos si el arma que se está es arma de fuego o cuerpo a cuerpo
         if (arma.canShoot == true)
         {
+            //Igualamos el numero de balas, rango y daño del scriptable object al script
             bullet = arma.bullet;
             range = arma.distance;
             damage = arma.damage;
